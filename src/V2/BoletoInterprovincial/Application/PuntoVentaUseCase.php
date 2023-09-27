131c1554f1f66ea5f25aaf6f396f798f4195bee1
Yosel Aguirre B <yaguirre@depilzone.com.pe>
2023-09-26 19:09:06 -0500

    26092023 1909

--- src/V2/BoletoInterprovincial/Application/PuntoVentaUseCase.php
+++ src/V2/BoletoInterprovincial/Application/PuntoVentaUseCase.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Src\V2\BoletoInterprovincial\Application;
 
+use Src\Core\Domain\ValueObjects\DateFormat;
 use Src\Core\Domain\ValueObjects\Id;
 use Src\Core\Domain\ValueObjects\NumericFloat;
 use Src\Core\Domain\ValueObjects\NumericInteger;
@@ -20,39 +21,93 @@ final class PuntoVentaUseCase
     }
 
     public function __invoke(
+
         string $idCliente,
         string $idSede,
+        string $idCaja,
+        int $idTipoDocumento,
+        string $numeroDocumento,
+        string $nombres,
+        string $apellidos,
+        int $menorEdad,
+
+
+        ?string $idVehiculo,
+        ?string $idAsiento,
+        ?string $fechaPartida,
+        ?string $horaPartida,
         string $idRuta,
         string $idParadero,
         float $precio,
-        int $idTipoDocumento,
-        string $numeroDocumento,
-        string $nombre,
-        ?string $direccion,
-        string $idUsuario
+        int $idTipoMoneda,
+        int $idFormaPago,
+        int $obsequio,
+
+        int $idTipoComprobante,
+        ?int $idTipoDocumentoEntidad,
+        ?string $numeroDocumentoEntidad,
+        ?string $nombreEntidad,
+        ?string $direccionEntidad,
+        string $idUsuarioRegistro
     ): void
     {
         $_idCliente = new Id($idCliente,false, 'El id del cliente no tiene el formato correcto');
         $_idSede = new Id($idSede,false, 'El id de la sede no tiene el formato correcto');
+        $_idCaja = new Id($idCaja,false, 'El id de la caja no tiene el formato correcto');
+        $_idTipoDocumento = new NumericInteger($idTipoDocumento);
+        $_numeroDocumento = new Text($numeroDocumento,false, -1, '');
+        $_nombres = new Text($nombres,false, -1, '');
+        $_apellidos = new Text($apellidos,false, -1, '');
+        $_menorEdad = new NumericInteger($menorEdad);
+
+
+        $_idVehiculo = new Id($idVehiculo,true, 'El id del vehiculo no tiene el formato correcto');
+        $_idAsiento = new Id($idAsiento,true, 'El id del asiento no tiene el formato correcto');
+        $_fechaPartida = new DateFormat($fechaPartida,true, 'El formato de la fecha no es la correcta');
+        $_horaPartida = new DateFormat($horaPartida,true, 'El formato de la hora no es la correcta');
         $_idRuta = new Id($idRuta,false, 'El id de la ruta no tiene el formato correcto');
         $_idParadero = new Id($idParadero,false, 'El id del paradero no tiene el formato correcto');
         $_precio = new NumericFloat($precio);
-        $_idTipoDocumento = new NumericInteger($idTipoDocumento);
-        $_numeroDocumento = new Text($numeroDocumento,false, -1, '');
-        $_nombre = new Text($nombre,false, -1, '');
-        $_direccion = new Text($direccion,true, -1, '');
-        $_idUsuario = new Id($idUsuario,false, 'El id del usuario no tiene el formato correcto');
+        $_idTipoMoneda = new NumericInteger($idTipoMoneda);
+        $_idFormaPago = new NumericInteger($idFormaPago);
+        $_obsequio = new NumericInteger($obsequio);
+
+        $_idTipoComprobante = new NumericInteger($idTipoComprobante);
+        $_idTipoDocumentoEntidad = new NumericInteger($idTipoDocumentoEntidad);
+        $_numeroDocumentoEntidad = new Text($numeroDocumentoEntidad,true, 4, 'El numero de documento de la entidad excede los 4 caracteres');
+        $_nombreEntidad = new Text($nombreEntidad,true, -1, '');
+        $_direccionEntidad = new Text($direccionEntidad,true, -1, '');
+
+        $_idUsuario = new Id($idUsuarioRegistro,false, 'El id del usuario no tiene el formato correcto');
 
         $this->repository->puntoVenta(
             $_idCliente,
             $_idSede,
+            $_idCaja,
+            $_idTipoDocumento,
+            $_numeroDocumento,
+            $_nombres,
+            $_apellidos,
+            $_menorEdad,
+
+
+            $_idVehiculo,
+            $_idAsiento,
+            $_fechaPartida,
+            $_horaPartida,
             $_idRuta,
             $_idParadero,
             $_precio,
-            $_idTipoDocumento,
-            $_numeroDocumento,
-            $_nombre,
-            $_direccion,
+            $_idTipoMoneda,
+            $_idFormaPago,
+            $_obsequio,
+
+            $_idTipoComprobante,
+            $_idTipoDocumentoEntidad,
+            $_numeroDocumentoEntidad,
+            $_nombreEntidad,
+            $_direccionEntidad,
+
             $_idUsuario
         );
     }
--- src/V2/BoletoInterprovincial/Infrastructure/PuntoVentaController.php
+++ src/V2/BoletoInterprovincial/Infrastructure/PuntoVentaController.php
@@ -24,27 +24,66 @@ final class PuntoVentaController
     public function __invoke( Request $request ): void
     {
         $user = Auth::user();
-        $idCliente = $request->id;
+
+        $idCliente = $request->input('idCliente');
         $idSede = $request->input('idSede');
+        $idCaja = $request->input('idCaja');
+        $idTipoDocumento = $request->input('idTipoDocumento');
+        $numeroDocumento = $request->input('numeroDocumento');
+        $nombres = $request->input('nombres');
+        $apellidos = $request->input('apellidos');
+        $menorEdad = $request->input('menorEdad');
+
+
+        $idVehiculo = $request->input('idVehiculo'); //null
+        $idAsiento = $request->input('numeroAsiento'); //null
+        $fechaPartida = $request->input('fechaPartida'); // null
+        $horaPartida = $request->input('horaSalida'); //null
         $idRuta = $request->input('idRuta');
         $idParadero = $request->input('idParadero');
         $precio = $request->input('precio');
-        $idTipoDocumento = $request->input('idTipoDocumento');
-        $numeroDocumento = $request->input('numeroDocumento');
-        $nombre = $request->input('nombre');
-        $direccion = $request->input('direccion');
+        $idTipoMoneda = $request->input('idTipoMoneda');
+        $idFormaPago = $request->input('idFormaPago');
+        $obsequio = $request->input('obsequio');
+
+        $idTipoComprobante = $request->input('idTipoComprobante');
+        $idTipoDocumentoEntidad = $request->input('idTipoDocumentoEntidad'); //null
+        $numeroDocumentoEntidad = $request->input('numeroDocumento');
+        $nombreEntidad = $request->input('nombreEntidad');
+        $direccionEntidad = $request->input('direccionEntidad');
+
+        $idUsuarioRegistro = $user->getId();
+
         $useCase = new PuntoVentaUseCase($this->repository);
         $useCase->__invoke(
             $idCliente,
             $idSede,
+            $idCaja,
+            $idTipoDocumento,
+            $numeroDocumento,
+            $nombres,
+            $apellidos,
+            $menorEdad,
+
+
+            $idVehiculo,
+            $idAsiento,
+            $fechaPartida,
+            $horaPartida,
             $idRuta,
             $idParadero,
             $precio,
-            $idTipoDocumento,
-            $numeroDocumento,
-            $nombre,
-            $direccion,
-            $user->getId()
+            $idTipoMoneda,
+            $idFormaPago,
+            $obsequio,
+
+            $idTipoComprobante,
+            $idTipoDocumentoEntidad,
+            $numeroDocumentoEntidad,
+            $nombreEntidad,
+            $direccionEntidad,
+
+            $idUsuarioRegistro,
         );
     }
 
