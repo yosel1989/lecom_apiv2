257a3b6ff9f26499f867bf0a42d8968218ad08f8
Yosel Aguirre <yaguirre@depilzone.com.pe>
2024-06-11 15:44:48 -0500

    listado de salidas por vehiculo y ruta, fecha

--- src/V2/CronogramaSalida/Infrastructure/GetListByVehiculoRutaFechaController.php
+++ src/V2/CronogramaSalida/Infrastructure/GetListByVehiculoRutaFechaController.php
@@ -4,8 +4,8 @@
 namespace Src\V2\CronogramaSalida\Infrastructure;
 
 use Illuminate\Http\Request;
-use Src\V2\CronogramaSalida\Application\GetCollectionByRutaUseCase;
-use Src\V2\CronogramaSalida\Domain\CronogramaSalidaList;
+use Src\V2\CronogramaSalida\Application\GetListByVehiculoRutaFechaUseCase;
+use Src\V2\CronogramaSalida\Domain\CronogramaSalidaShortList;
 use Src\V2\CronogramaSalida\Infrastructure\Repositories\EloquentCronogramaSalidaRepository;
 
 final class GetListByVehiculoRutaFechaController
@@ -21,12 +21,12 @@ final class GetListByVehiculoRutaFechaController
      * @param Request $request
      * @return mixed
      */
-    public function __invoke( Request $request ): CronogramaSalidaList
+    public function __invoke( Request $request ): CronogramaSalidaShortList
     {
         $idVehiculo = $request->idVehiculo;
         $idRuta = $request->idRuta;
         $fecha = (new \DateTime('now'))->format('Y-m-d');
-        $useCase = new GetCollectionByRutaUseCase($this->repository);
+        $useCase = new GetListByVehiculoRutaFechaUseCase($this->repository);
         return $useCase->__invoke($idVehiculo, $idRuta, $fecha);
     }
 
